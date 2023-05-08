213ddaac262b1b804cd4b2f6cbb6386354c41835
purplegem903 <82189117+purplegem903@users.noreply.github.com>
2023-05-08 23:32:10 +0100

    Add comments for tasks app python code

--- week-3-django/lecture3/lecture3/tasks/urls.py
+++ week-3-django/lecture3/lecture3/tasks/urls.py
@@ -1,5 +1,12 @@
+# Import necessary modules and classes from Django
 from django.urls import path
 from . import views
 
+# Define the namespace for the tasks app
 app_name = "tasks"
-urlpatterns = [path("", views.index, name="index"), path("add", views.add, name="add")]
+
+# Define the URL patterns for the tasks app
+urlpatterns = [
+    path("", views.index, name="index"),  # Maps the root URL to the index view
+    path("add", views.add, name="add"),  # Maps the "/add" URL to the add view
+]
--- week-3-django/lecture3/lecture3/tasks/views.py
+++ week-3-django/lecture3/lecture3/tasks/views.py
@@ -4,24 +4,38 @@ from django.shortcuts import render
 from django.urls import reverse
 
 
+# Define a form for creating new tasks
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
 
-# Create your views here.
+# View function for the index page
 def index(request):
+    # Check if "tasks" key exists in session
     if "tasks" not in request.session:
+        # If not, initialize an empty list for tasks
         request.session["tasks"] = []
+    # Render the index.html template with the tasks data from session
     return render(request, "tasks/index.html", {"tasks": request.session["tasks"]})
 
 
+# View function for adding a new task
 def add(request):
+    # Check if the request method is POST (form submission)
     if request.method == "POST":
+        # Create a form instance with the POST data
         form = NewTaskForm(request.POST)
+        # Check if the form data is valid
         if form.is_valid():
+            # Extract the task from the form's cleaned data
             task = form.cleaned_data["task"]
+            # Append the task to the tasks list in session
             request.session["tasks"] += [task]
+            # Redirect the user to the index page
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
+            # If the form is not valid, render the add.html template with the form data
             return render(request, "tasks/add.html", {"form": form})
+    # If the request method is not POST, create a new form instance
+    # and render the add.html template with the form data
     return render(request, "tasks/add.html", {"form": NewTaskForm()})
