03fb0ef539c1bceed53cfcf92d97152c0a127046
purplegem903 <82189117+purplegem903@users.noreply.github.com>
2023-05-16 02:06:04 +0100

    Finish Project 2 (commerce/auctions)

--- week-4-sql-models-migrations/project2/commerce/auctions/admin.py
+++ week-4-sql-models-migrations/project2/commerce/auctions/admin.py
@@ -1,8 +1,13 @@
+# This imports the Django admin module.
 from django.contrib import admin
+
+# This imports the models from the auctions app.
 from .models import Listing, Bid, Comment, Watchlist, User
 
 
+# This creates a new class that inherits from the Django admin ModelAdmin class.
 class ListingAdmin(admin.ModelAdmin):
+    # This specifies the fields that will be displayed in the admin list view.
     list_display = (
         "title",
         "description",
@@ -15,14 +20,29 @@ class ListingAdmin(admin.ModelAdmin):
     )
 
 
+# This creates a new class that inherits from the Django admin ModelAdmin class.
 class UserAdmin(admin.ModelAdmin):
+    # This specifies the fields that will be displayed in the admin list view.
     list_display = ("username", "email", "is_staff", "is_active", "date_joined")
+
+    # This specifies the fields that will be used to filter the admin list view.
     list_filter = ("is_staff", "is_active")
+
+    # This specifies the fields that can be searched in the admin list view.
     search_fields = ("username", "email")
 
 
+# This registers the Listing model with the Django admin site.
 admin.site.register(Listing, ListingAdmin)
+
+# This registers the Watchlist model with the Django admin site.
 admin.site.register(Watchlist)
+
+# This registers the User model with the Django admin site, using the UserAdmin class.
 admin.site.register(User, UserAdmin)
+
+# This registers the Comment model with the Django admin site.
 admin.site.register(Comment)
+
+# This registers the Bid model with the Django admin site.
 admin.site.register(Bid)
--- week-4-sql-models-migrations/project2/commerce/auctions/forms.py
+++ week-4-sql-models-migrations/project2/commerce/auctions/forms.py
@@ -4,10 +4,16 @@ from .models import Listing, Comment, Bid
 
 
 class CreateListingForm(forms.Form):
-    title = forms.CharField(label="Title", max_length=200)
+    """
+    Form for creating a new listing.
+    """
+
+    title = forms.CharField(
+        label="Title", max_length=200
+    )  # Field for the title of the listing
     description = forms.CharField(
         label="Description", max_length=500, widget=forms.Textarea(attrs={"rows": 4})
-    )
+    )  # Field for the description of the listing
     starting_bid = forms.DecimalField(
         label="Starting Bid",
         decimal_places=2,
@@ -15,18 +21,24 @@ class CreateListingForm(forms.Form):
         widget=forms.NumberInput(
             attrs={"placeholder": "$", "type": "number", "step": "0.01"}
         ),
-    )
+    )  # Field for the starting bid of the listing
     image = forms.URLField(
         label="Image URL", required=False, widget=forms.URLInput(attrs={"size": 40})
-    )
+    )  # Field for the image URL of the listing
 
-    category = forms.ChoiceField(label="Category", choices=Listing.CATEGORY_CHOICES)
+    category = forms.ChoiceField(
+        label="Category", choices=Listing.CATEGORY_CHOICES
+    )  # Field for selecting the category of the listing
 
 
 class CommentForm(forms.ModelForm):
+    """
+    Form for adding a comment to a listing.
+    """
+
     class Meta:
-        model = Comment
-        fields = ("text",)
+        model = Comment  # Associate the form with the Comment model
+        fields = ("text",)  # Include only the 'text' field from the Comment model
         widgets = {
             "text": forms.Textarea(
                 attrs={
@@ -35,10 +47,14 @@ class CommentForm(forms.ModelForm):
                     "class": "form-control comment-form",
                 }
             ),
-        }
+        }  # Customize the 'text' field widget appearance
 
 
 class BidForm(forms.ModelForm):
+    """
+    Form for placing a bid on a listing.
+    """
+
     amount = forms.FloatField(
         widget=forms.TextInput(
             attrs={
@@ -46,8 +62,8 @@ class BidForm(forms.ModelForm):
                 "placeholder": "Bid",
             }
         )
-    )
+    )  # Field for entering the bid amount
 
     class Meta:
-        model = Bid
-        fields = ("amount",)
+        model = Bid  # Associate the form with the Bid model
+        fields = ("amount",)  # Include only the 'amount' field from the Bid model
